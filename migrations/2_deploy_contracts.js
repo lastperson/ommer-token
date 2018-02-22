10aa7a287a598cbbf2148a43add6ff0ec7d90dfe
Tomas Libal <tomas@libal.eu>
2018-02-22 11:43:24 +0800

    add SafetyWallet to the migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 let OmmerToken = artifacts.require('./OmmerToken.sol')
 let OmmerCrowdsale = artifacts.require('./OmmerCrowdsale.sol')
 let OmmerTeamVesting = artifacts.require('./OmmerTeamVesting.sol')
+let SafetyWallet = artifacts.require('./SafetyWallet.sol')
 
 module.exports = function(deployer, network, accounts) {
     deployer.deploy(OmmerToken).then(function() {
@@ -12,4 +13,6 @@ module.exports = function(deployer, network, accounts) {
       var exchangeRate = 400; // 1 OMR = 0.0025 ETH or 1 ETH = 400 OMR
       return deployer.deploy(OmmerCrowdsale, exchangeRate, accounts[0], OmmerToken.address);
     });
+
+    deployer.deploy(SafetyWallet, "0x1234");
 };
