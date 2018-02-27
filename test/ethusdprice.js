9a2bb45cfafe15149870c6ef26f2d9aabc80234d
Tomas Libal <tomas@libal.eu>
2018-02-27 17:12:19 +0800

    add test for EthUsdPrice initial conditions

---
+++ test/ethusdprice.js
@@ -0,0 +1,30 @@
+var EthUsdPrice = artifacts.require("./EthUsdPrice.sol");
+
+contract("EthUsdPrice", function(accounts) {
+  let token;
+  const creator = accounts[0];
+  const joe = accounts[1];
+
+  beforeEach(async function () {
+    ethUsdPrice = await EthUsdPrice.new({ from: creator });
+  });
+
+  it('starts uninitialised', async function () {
+    const inited = await ethUsdPrice.ethPriceInitialised();
+    assert.isFalse(inited);
+  });
+
+  it('starts with price 0', async function () {
+    const cents = await ethUsdPrice.ethInCents();
+    assert.equal(cents, 0);
+  });
+
+  it.skip('should issue new request to oraclize', async function () {
+    try {
+      await ethUsdPrice.update({ value: web3.toWei(1, 'ether') });
+    } catch (e) {
+      assert.fail(e);
+    }
+  });
+
+});
