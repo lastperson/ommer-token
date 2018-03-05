dd3dd22305ef9c92065819fbc36f4df2968ceb02
Tomas Libal <tomas@libal.eu>
2018-03-05 10:38:31 +0800

    swap the ICO contracts
    - old was using ETH accounting,
    - the new one is using $US cent accounting

---
+++ contracts/OmmerIco.sol
@@ -0,0 +1,188 @@
+/**
+ *   ___  _ __ ___  _ __ ___   ___ _ __
+ *  / _ \| '_ ` _ \| '_ ` _ \ / _ \ '__|
+ * | (_) | | | | | | | | | | |  __/ |
+ *  \___/|_| |_| |_|_| |_| |_|\___|_|
+ *
+ * https://www.ommer.com
+ */
+pragma solidity 0.4.18;
+
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
+import "zeppelin-solidity/contracts/lifecycle/Pausable.sol";
+import "./EthUsdPrice.sol";
+import "./OmmerToken.sol";
+
+
+contract OmmerIco is EthUsdPrice, Pausable {
+
+    /** ######################################################################
+     *
+     *  A T T A C H   L I B R A R Y   F U N C T I O N S
+     *
+     * ######################################################################
+     */
+    using SafeMath for uint256;
+
+
+    /** ######################################################################
+     *
+     *  S T O R A G E
+     *
+     * ######################################################################
+     */
+
+    /**
+     * This is the token being sold in this ICO
+     */
+    OmmerToken public omr;
+
+    /**
+     * Number of remaining OMR tokens for sale
+     *
+     * Note: The number is in OMR which has 10 ** 18 decimals.
+     */
+    uint256 private _remainingOmr;
+
+    /**
+     * Fund Sink is a EOA where the proceeds of the crowdsale are
+     * forwarded to.
+     */
+    address private _fundSink;
+
+    /**
+     * Contribution tracks individual address' value (in OMR) sent to this
+     * contract. All OMR are tracked as unconfirmed first. Only after the owner
+     * of this ICO contract verifies that the KYC requirements have been met for
+     * the particular address, then the owner calls a function which transfers
+     * the unconfirmed balance to the address.
+     *
+     * Both numbers are in OMR which has 10 ** 18 decimals.
+     */
+    struct Contribution {
+        uint256 unconfirmed;
+        uint256 withdrawn;
+    }
+
+    /**
+     * Tracks contribution for each address
+     */
+    mapping(address => Contribution) public contributions;
+
+
+    /** ######################################################################
+    *
+    *  E V E N T S
+    *
+    * ######################################################################
+    */
+
+
+    /** ######################################################################
+     *
+     *  F U N C T I O N S
+     *
+     * ######################################################################
+     */
+
+    function OmmerIco(uint256 _initialEthUsdCentsPrice, address _omr, address _ommerEOA) public {
+        ethInCents = _initialEthUsdCentsPrice;
+        paused = true;
+        omr = OmmerToken(_omr);
+        _fundSink = _ommerEOA;
+    }
+
+    /**
+     * incoming transfer: fallback function
+     *
+     * when someone just sends Ether to the contract address without
+     * invoking the purchaseTokens() function, we trigger the purchaseTokens
+     * function.
+     * NB: as this is a standard transaction the purchaseTokens function
+     * should not exceed the std. tx gas limit.
+     */
+    function () public payable {
+        purchaseTokens(msg.sender);
+    }
+
+    /**
+     * Purchasing means allocating a portion of the unsold OMR tokens to the
+     * _beneficiary (which is the sending address).
+     *
+     *  1) 1 OMR = 1 USD, so we first convert the sent wei into USD
+     *  2) Then check if the needed number of OMR is still available
+     *  3) Add the OMR number to the contribution list under the sender's address
+     *  4) Deduct the OMR number from the available OMR for sale
+     */
+    function purchaseTokens(address _beneficiary) public whenNotPaused payable {
+        require(getMinPossibleContribution() <= msg.value);
+
+        uint256 omrTokens = getUsdCentValue(msg.value) / 100 * 10 ** omr.decimals();
+
+        require(getRemainingTokensForSale() >= omrTokens);
+
+        Contribution storage c = contributions[_beneficiary];
+
+        c.unconfirmed = c.unconfirmed.add(omrTokens);
+
+        _remainingOmr.sub(omrTokens);
+
+        forwardFunds();
+    }
+
+    /**
+     * When starting the sale, the contract checks how many tokens have been
+     * allocated for this contract to sell.
+     */
+    function unpause() public onlyOwner whenPaused {
+        _remainingOmr = omr.balanceOf(this);
+        assert(_remainingOmr > 0);
+
+        super.unpause();
+    }
+
+    function verify(address _beneficiary) public onlyOwner {
+        Contribution storage c = contributions[_beneficiary];
+
+        uint256 omrToSend = c.unconfirmed;
+
+        require(omrToSend > 0);
+
+        c.unconfirmed = 0;
+        c.withdrawn = c.withdrawn.add(omrToSend);
+
+        omr.transfer(_beneficiary, omrToSend);
+    }
+
+    /**
+     * Returns the equivalent value in USD cents for the value of wei sent.
+     *
+     * E.g. if 1 ETH = 825.25 USD which is 82,525 cents then 0.1 ETH which is
+     * 100000000000000000 wei will be worth 82.52 USD or 8252 cents.
+     *
+     * NOTE: there's no rounding!
+     */
+    function getUsdCentValue(uint256 _wei) public view returns (uint256) {
+        return (_wei * 100 / 1 ether * ethInCents / 100);
+    }
+
+    function getRemainingTokensForSale() public view returns (uint256) {
+        return _remainingOmr;
+    }
+
+    /**
+     * Returns the minimum possible contribution in wei.
+     * The minimum is 0.01 ETH ().
+     */
+    function getMinPossibleContribution() public constant returns (uint256) {
+        return 1 ether / 100;
+    }
+
+    /**
+     * Because this is a non-EOA contract, we need to pass on the contributed
+     * funds to an address where Ommer can control them.
+     */
+    function forwardFunds() internal {
+        _fundSink.transfer(msg.value);
+    }
+}
---
+++ test/ico.js
@@ -0,0 +1,140 @@
+var OmmerIco = artifacts.require("./OmmerIco.sol");
+var OmmerToken = artifacts.require("./OmmerToken.sol");
+
+contract("OmmerIco", function(accounts) {
+  let ommmerIco;
+  let omr;
+  let omrDecimals;
+  const creator = accounts[0];
+  const joe = accounts[1];
+  const fundSink = accounts[2];
+
+  beforeEach(async () => {
+    omr = await OmmerToken.new({ from: creator });
+    omrDecimals = await omr.decimals();
+    ommerIco = await OmmerIco.new(85025, omr.address, fundSink, { from: creator });
+  });
+
+  it("should start paused", async () => {
+    const isPaused = await ommerIco.paused();
+    assert.equal(isPaused, true, "ico should not be running");
+  });
+
+  it("should throw if unpausing ICO without tokens to sell", async () => {
+    try {
+      await ommerIco.unpause({ from: creator });
+      assert.fail("unpausing should have thrown");
+    } catch (err) {
+    }
+  });
+
+  it("should unpause if ICO has tokens to sell", async () => {
+    await omr.transfer(ommerIco.address, 10000 * 10 ** omrDecimals, { from: creator });
+    await ommerIco.unpause({ from: creator });
+  });
+
+  it("should pause a running ICO if called by the owner", async () => {
+    await omr.transfer(ommerIco.address, 10000 * 10 ** omrDecimals, { from: creator });
+    await ommerIco.unpause({ from: creator });
+    await ommerIco.pause({ from: creator });
+    const isPaused = await ommerIco.paused();
+    assert.isTrue(isPaused);
+  });
+
+  it("should throw if unpausing ICO with tokens to sell but called by non-owner", async () => {
+    try {
+      await omr.transfer(ommerIco.address, 10000 * 10 ** omrDecimals, { from: creator });
+      await ommerIco.unpause({ from: joe });
+      assert.fail("unpausing should have thrown");
+    } catch (err) {
+    }
+  });
+
+  it("should return 8502 cents for 0.1 ETH value", async () => {
+    const cents = await ommerIco.getUsdCentValue(100000000000000000);
+    assert.equal(cents, 8502);
+  });
+
+  it('should report 0.01 ETH as the minimum for contribution', async () => {
+    const min = 10 ** 18 / 100;
+    const minContribution = await ommerIco.getMinPossibleContribution();
+    assert.equal(minContribution.toNumber(), min);
+  });
+
+  it("should forward the ICO proceeds to the fund sink address", async () => {
+    await omr.transfer(ommerIco.address, 10000 * 10 ** omrDecimals, { from: creator });
+    await ommerIco.unpause();
+    const oBalance = await web3.eth.getBalance(fundSink);
+    await web3.eth.sendTransaction({from: joe, to: ommerIco.address, value: web3.toWei(0.5, 'ether'), gas: 1000000 });
+    const nBalance = await web3.eth.getBalance(fundSink).toNumber();
+    assert.equal(nBalance, oBalance.add(web3.toWei(0.5, 'ether')).toNumber());
+  });
+
+  it("should accept value if value > minValue and ICO is running", async () => {
+    await omr.transfer(ommerIco.address, 10000 * 10 ** omrDecimals, { from: creator });
+    await ommerIco.unpause();
+    await web3.eth.sendTransaction({from: joe, to: ommerIco.address, value: web3.toWei(0.5, 'ether'), gas: 1000000 });
+    await web3.eth.sendTransaction({from: joe, to: ommerIco.address, value: web3.toWei(0.2, 'ether'), gas: 1000000 });
+    const c = await ommerIco.contributions(joe);
+    const unconfirmed = c[0].toNumber();
+    const withdrawn = c[1].toNumber();
+    assert.equal(unconfirmed, Math.round(0.7 * 850.25) * 10 ** omrDecimals);
+    assert.equal(withdrawn, 0);
+  });
+
+  it("should throw if verify called by non-owner", async () => {
+    try {
+      await omr.transfer(ommerIco.address, 10000 * 10 ** omrDecimals, { from: creator });
+      await ommerIco.unpause();
+      await web3.eth.sendTransaction({from: joe, to: ommerIco.address, value: web3.toWei(0.5, 'ether'), gas: 1000000 });
+      await ommerIco.verify(joe, { from: joe });
+      assert.fail("should have thrown");
+    } catch (err) {
+      // all good if the correct error...
+    }
+  });
+
+  it("should throw if contributor has no OMR in the ICO contract", async () => {
+    await omr.transfer(ommerIco.address, 10000 * 10 ** omrDecimals, { from: creator });
+    await ommerIco.unpause({ from: creator });
+    try {
+      await ommerIco.verify(joe, { from: creator });
+      assert.fail("should have thrown");
+    } catch (err) {
+      //
+      const joeOmr = await omr.balanceOf(joe);
+      assert.equal(joeOmr, 0);
+    }
+  });
+
+  it("should disperse the OMR to the contributor upon verifying them", async () => {
+    await omr.transfer(ommerIco.address, 10000 * 10 ** omrDecimals, { from: creator });
+    await ommerIco.unpause({ from: creator });
+    await web3.eth.sendTransaction({from: joe, to: ommerIco.address, value: web3.toWei(0.5, 'ether'), gas: 1000000 });
+    await ommerIco.verify(joe, { from: creator });
+    const joeOmr = await omr.balanceOf(joe);
+    assert.equal(joeOmr, Math.round(0.5 * 850.25) * 10 ** omrDecimals);
+  });
+
+  it("should throw if paused ICO is sent a contribution ", async () => {
+    try {
+      await omr.transfer(ommerIco.address, 10000 * 10 ** omrDecimals, { from: creator });
+      await web3.eth.sendTransaction({from: joe, to: ommerIco.address, value: web3.toWei(0.5, 'ether'), gas: 1000000 });
+      assert.fail("should have thrown");
+    } catch (err) {
+      // all good if the correct error...
+    }
+  });
+
+  it("should throw if remaining OMR < contribution ", async () => {
+    try {
+      await omr.transfer(ommerIco.address, 100 * 10 ** omrDecimals, { from: creator });
+      await ommerIco.unpause({ from: creator });
+      await web3.eth.sendTransaction({from: joe, to: ommerIco.address, value: web3.toWei(0.5, 'ether'), gas: 1000000 });
+      assert.fail("should have thrown");
+    } catch (err) {
+      // all good if the correct error...
+    }
+  });
+
+});
